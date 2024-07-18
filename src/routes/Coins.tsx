2671099467f4667603c0e6f863069ea55287855f
Rayched <ny2401@naver.com>
2024-07-18 17:52:22 +0900

    2024.07.18 : Coin Data fetch, Home 출력, Icon 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+//Main Home CSS Style
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -22,8 +26,10 @@ const Coin = styled.li`
     margin-bottom: 10px;
     a {
         padding: 20px;
-        display: block;
+        display: flex;
+        align-items: center;
         font-size: 18px;
+        font-weight: bold;
     }
     &:hover {
         color: ${(props) => props.theme.accentColor};
@@ -31,58 +37,72 @@ const Coin = styled.li`
     };
 `;
 
+const CoinImgs = styled.img`
+    width: 30px; height: 30px;
+    margin-right: 7px;
+`;
+
 const Title = styled.h2`
     color: ${(props) => props.theme.accentColor};
     font-size: 30px;
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },  
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },  
-];
+//Coin Data's Type Setting
+interface CoinData {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+};
 
 function Coins(){
+    const [coins, setCoins] = useState<CoinData[]>([]);
+    const [Loading, setLoading] = useState(true);
+
+    //Coin Data 가져오기
+    useEffect(() => {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            //console.log(json);
+            setCoins(json.slice(0, 50));
+            setLoading(false);
+        })();
+    }, []);
+
+    console.log(coins);
+
     return (
         <Container>
             <Header>
                 <Title>코인 목록</Title>
             </Header>
-            <CoinList>
-                {
-                    coins.map((coin) => 
-                        <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>
-                            {coin.name}
-                            </Link>
-                        </Coin>
-                    )
-                }
-            </CoinList>
+            {
+                Loading ? 
+                (
+                    <div>
+                        <h3>코인(암호화폐) 데이터를 가져오고 있습니다.</h3>
+                        <h3>잠시만 기다려주세요...</h3>
+                    </div>
+                )
+                : (
+                    <CoinList>
+                    {
+                        coins.map((coin) => 
+                            <Coin key={coin.id}>
+                                <Link to={`/${coin.id}`}>
+                                    <CoinImgs src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}/>
+                                    {coin.name}
+                                </Link>
+                            </Coin>
+                        )
+                    }
+                    </CoinList> 
+                )
+            }
         </Container>
     );
 }
